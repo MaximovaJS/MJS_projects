1396672184970b596bd4dd641a88d3177b4eb16b
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-11-18 22:36:28 +0300

    Выполнено

--- homeworks/task_05.py
+++ homeworks/task_05.py
@@ -22,3 +22,16 @@ for i in month:
 credit=round(abs(salary*12-all_exp),2)
 
 print('Необходимо взять в долг', credit, 'рублей')
+
+# Хорошо. Можно еще через while
+# Решение 2
+salary, expenses = 10000, 12000
+
+i = expenses - salary
+m = 0
+debt = 0
+while m < 12:
+    mx = i * (1.03 ** m)
+    debt += mx
+    m += 1
+print(f'Необходимо взять в долг {round(debt, 2)} рублей')
